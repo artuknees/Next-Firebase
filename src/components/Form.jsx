85760830d4c440ea5b4a3e027fed24d5ac737b5e
artuknees <artuknees@gmail.com>
2023-02-21 01:26:08 -0300

    patch request

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,8 +9,8 @@ const Form = ({productos}) => {
     const db = getFirestore(firebaseApp);
     const [thing , setThing] = useState({
         nombre: '',
-        precio: 0,
-        cantidad: 0 
+        precio: '',
+        cantidad: '' 
     })
     const handleSubmit = async (e) => { // postData
         e.preventDefault();
@@ -18,25 +18,36 @@ const Form = ({productos}) => {
             await addDoc(collection(db, 'producto'),{
                 ...thing
             });
-            router.push('/')
+            router.reload()
         } catch (err) {
             console.error(err)
         }
     };
     const handleDetails = (id) => {
         router.push(`/products/${id}`)
-
     }
     return (
-        <div className="flex flex-row w-full justify-evenly">
+        <div className="flex flex-row w-full justify-evenly pt-5">
             <form className="flex flex-col w-48 items-center mx-24 py-12 border border-black" onSubmit={(e) => handleSubmit(e)}>
-                <label className="bg-slate-400">Ingrese su nombre</label>
-                <input type="text" onChange={(e) => setThing({...thing, nombre: e.target.value})} className="w-full border-b border-b-slate-200 mt-4"/>
-                <label className="bg-slate-400">Ingrese precio</label>
-                <input type="text" onChange={(e) => setThing({...thing, precio: parseInt(e.target.value)})} className="w-full border-b border-b-slate-200 mt-4"/>
-                <label className="bg-slate-400">Ingrese cantidad</label>
-                <input type="text" onChange={(e) => setThing({...thing, cantidad: parseInt(e.target.value)})} className="w-full border-b border-b-slate-200 mt-4"/>
-                <button type="submit" className="w-full bg-teal-500 mt-4">Enviar</button>
+                <label className="bg-slate-400">Ingrese el nombre</label>
+                <input type="text" 
+                    defaultValue={thing.nombre} 
+                    onChange={(e) => setThing({...thing, nombre: e.target.value})} 
+                    className="border-b border-b-slate-200 mt-4 w-5/6"
+                />
+                <label className="bg-slate-400 mt-4">Ingrese precio</label>
+                <input type="text" 
+                    defaultValue={thing.precio} 
+                    onChange={(e) => setThing({...thing, precio:e.target.value})} 
+                    className="w-5/6 border-b border-b-slate-200 mt-4"
+                />
+                <label className="bg-slate-400 mt-4">Ingrese cantidad</label>
+                <input type="text" 
+                    defaultValue={thing.cantidad} 
+                    onChange={(e) => setThing({...thing, cantidad: e.target.value})} 
+                    className="w-5/6 border-b border-b-slate-200 mt-4"
+                />
+                <button type="submit" className="w-5/6 bg-teal-500 mt-4">Enviar</button>
             </form>
             <div className="flex flex-col items-center justify-center w-1/3">
                 {productos?.length > 0 && productos.map(item => {return(
--- src/components/SimpleProduct.jsx
+++ src/components/SimpleProduct.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import { useRouter } from "next/router";
 import firebaseApp from "../../firebase";
-import { getFirestore , collection , addDoc , getDocs , doc ,deleteDoc , getDoc , setDoc } from 'firebase/firestore';
+import { getFirestore , collection , addDoc , getDocs , doc ,deleteDoc , getDoc , setDoc , updateDoc } from 'firebase/firestore';
 import { async } from '@firebase/util'; 
 const db = getFirestore(firebaseApp);
 
 
 
 const SimpleProduct = ({product}) => {
+    const [newName, setNewName] = useState('')
     const router = useRouter();
     const { query } = useRouter();
 
@@ -16,6 +17,14 @@ const SimpleProduct = ({product}) => {
         await deleteDoc(doc(db,'producto',product))
         router.push('/'); // me voy a home despues de borrar
     };
+
+    const handleChangeName = async () => {
+        const { product } = query;
+        await updateDoc(doc(db,'producto',product),{
+            nombre: newName
+        });
+        router.push('/')
+    };
     return (
         <div className="flex flex-col w-1/4 items-center">
             <p>{`Name: ${product.nombre}`}</p>
@@ -23,6 +32,9 @@ const SimpleProduct = ({product}) => {
             <p>{`Amount: ${product.cantidad}`}</p>
             <button className="bg-pink-500" onClick={() => {router.push('/')}}> volver atras</button>
             <button className="bg-yellow-500 mt-5" onClick={() => deleteProduct()}>borrar producto</button>
+            <label htmlFor="">escribir nuevo nombre</label>
+            <input type="text" className="border-b" onChange={(e) => setNewName(e.target.value)} />
+            <button className="border border-blue-800 mt-4 cursor-pointer" onClick={() => handleChangeName()}>change name</button>
         </div>
     )
 };
